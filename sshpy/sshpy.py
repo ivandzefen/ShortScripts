a0f7f7d9e8d82e8eac6bb1d4f983afcc5063fd73
Ivan-F3nDz3 <ivanfenyom@gmail.com>
2020-10-17 17:13:19 +0100

    Added a script to communicate with remote server using SSH, given IP, PORT and Server Password

---
+++ sshpy/sshpy.py
@@ -0,0 +1,48 @@
+import paramiko
+import sys
+''' sshpy
+    python
+    usage : python3 sshpy.py 'hostname' 'username' 'password'(optional) 'port(optional)'''
+def main() :
+    client=paramiko.SSHClient()
+    client.load_system_host_keys()
+    args=sys.argv[1:]
+    connect_info={'hostname':None,
+                  'username':None,
+                  'password':None,
+                  'port':22,
+                  'allow_agent':False,
+                  'look_for_keys':False}
+    l=len(args)
+    if l<2 :
+        print('too few arguements')
+        return()
+    elif l>4 :
+        print('too many arguements')
+        return
+    else :
+        for i in range(l) :
+            for key in connect_info:
+                if connect_info[key]==None :
+                    connect_info[key]=args[i]
+                    break
+                elif connect_info[key]==22:
+                    connect_info[key]=int(args[i])
+                    break
+    try :
+        client.connect(**connect_info)
+    except Exception as ex :
+        print(ex)
+        return
+    print('welcome to sshpy (enter exit to quit)')
+    while True :
+        cmd=str(input(connect_info['username']+'@'+connect_info['hostname']+':'))
+        if cmd=='exit':
+            client.close()
+            return
+        stdin, stdout, stderr=client.exec_command(cmd)
+        print(stdout.read().decode("utf8")+' '+stderr.read().decode("utf8"))
+
+
+if __name__=='__main__' :
+    main()
